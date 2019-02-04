c63bd00b02ec4dff34716898316dc1715755b546
svmihar <svmihar@gmail.com>
2019-02-04 14:48:52 +0700

    add preproses pipeline

--- preproses.py
+++ preproses.py
@@ -1,52 +1,53 @@
 import similarity
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 import pandas as pd 
+from pandas import ExcelWriter
+from pandas import ExcelFile
 
 # create stemmer
 factory = StemmerFactory()
 stemmer = factory.create_stemmer()
 
-# ---Preprocess 
-kompas=pd.read_csv(r'./csv/kompas.csv')
-kompas_content = kompas['content'].values
-print(f'length of kompas\' document: {len(kompas_content)}')
-kompas_judul = kompas['title'].values
-
-bisnis = pd.read_csv(r'./csv/bisnis.csv')
-bisnis_content = bisnis['content'].values
-bisnis_judul=bisnis['title'].values
  
-kontan = pd.read_csv(r'./csv/kontan.csv')
-kontan_content = kontan['content'].values
-kontan_judul=kontan['title'].values
+df = pd.read_excel('dataframe.xlsx')
+# print(df.columns)
+isi = df['content'].values
+judul = df['title'].values
 
-def stemisasi(str):
-    return stemmer.stem(str)
+hasil_isi = []
+hasil_judul = []
 
-kumpulan_berita = [kompas,bisnis,kontan]
-for berita in kumpulan_berita:
-    print(berita['content'].notnull())
-    berita.dropna(inplace=True)
-    berita['judul_stem'] = [stemisasi(kata) for kata in berita['title'].values]
-    berita['content_stem'] = [stemisasi(kata) for kata in berita['content'].values]
-    print(berita)
-def ke_csv(dataframe,name='test.csv'):
-    dataframe.to_csv('csv/'+name)
-options = {0:'kompas_cleaned',1:'bisnis_cleaned','2':'kontan_cleaned'}
 
-for index, item in enumerate(kumpulan_berita):
-    ke_csv(item,name='csvke{}.csv'.format(options.get(index))
+for berita,titel in zip(isi,judul):
+    # hasil_isi.append(stemmer.stem(berita))
+    try:
+        x = stemmer.stem(berita)
+        y = stemmer.stem(titel)
+        hasil_judul.append(y)
+        hasil_isi.append(x)
+        print(f'stemming \n{titel} to \n{y}')
+        print(f'stemming \n{berita} to \n{x}')
+    except: 
+        print('gagal')
+
+df['titel_stem'] = hasil_judul
+df['content_stem'] = hasil_isi
+
+df.to_excel('dataframe_preprocessed.xlsx')
+
 
 
 # NGRAM METHOD BERHASIL
-from similarity.ngram import NGram
+# from similarity.ngram import NGram
+""" from similarity.ngram import NGram
 jum_gram = 2
 batas = 0.5 #threshold
 gram = NGram(jum_gram)
 
-for index_kompas, judul_kompas in enumerate(kontan_judul):
-    for index_bisnis,judul_bisnis in enumerate(bisnis_judul): 
-        jarak = gram.distance(stemmer.stem(judul_kompas),stemmer.stem(judul_bisnis))
+for index_kompas, judul_kompas in enumerate(kompas_judul):
+    for index_bisnis,judul_bisnis in enumerate(kontan_judul): 
+        jarak = gram.distance(judul_kompas,judul_bisnis)
         if batas>jarak:
             # print(judul)
             print(f'jarak antar berita dengan judul \'{judul_kompas}\' dengan berita di bisnis dengan judul \'{judul_bisnis}\' adalah: \n{jarak}')
+ """
--- readme.md
+++ readme.md
@@ -1,1 +1,13 @@
 ![img](/img/crawler.gif)
+
+# scraper 
+- 3 news source
+  - kontan 
+  - bisnis 
+  - kompas
+
+- scraped: 
+  - image
+  - title
+  - date, time
+  - content paragraph
